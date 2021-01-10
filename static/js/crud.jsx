d5a21e778f05a6f2ab7e013396d9ac8c1d0c89e7
Ashley Kwan <ashleykwan888@gmail.com>
2021-01-09 21:16:25 -0800

    added useState()

--- static/js/crud.jsx
+++ static/js/crud.jsx
@@ -1,1 +1,25 @@
+// function addGoal(name) {
+//     alert(name);
+// }
 
+// event handling for "Add" button
+{/* <button type="button" onClick={ () =>
+alert("Added New Goal!")
+}>Add</button>
+
+function handleSubmit(event) {
+    event.preventDefault();
+    PaymentResponse.addGoal("Added New Goal!")
+} */}
+
+// stating the initial state of goals
+const [goals, setGoals] = useState(props.goals);
+
+const goalsList = goals.map(goal => (
+    <Goal 
+        id ={goal.id}
+        name={goal.name}
+        completed={goal.completed}
+    />
+    )
+);
