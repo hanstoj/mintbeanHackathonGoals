9833c0631403835c2ae55db2316b89a7c925a7ed
Ashley Kwan <ashleykwan888@gmail.com>
2021-01-09 16:31:40 -0800

    added CRUD.jsx file

---
+++ static/js/crud.jsx
@@ -0,0 +1,13 @@
+// establish variables that are linked to he HTML
+// const form = document.getElementById("form");
+// const button = document.getElementById("button");
+// const todo = document.getElementById("todo");
+
+// let todoList = []; 
+
+ // event handling when added a new todo item
+// form.addEventListener("submit", function (event) {
+     // cancel an event; validate the input before submitting
+//     event.preventDefault();
+//     addTodo();
+// }); 
